4cb21d8fbb65137c71e727193981a47fc4fc6d93
6410110171 <owen55660@gmail.com>
2024-07-06 15:44:22 +0700

    Instance Variables

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({
+  const StyledText(
+    this.text,{
     super.key,
   });
+  
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Hello World!',
-      style: TextStyle(
+    return Text(
+      text,
+      style: const TextStyle(
         color: Colors.white,
         fontSize: 28,
       ),
